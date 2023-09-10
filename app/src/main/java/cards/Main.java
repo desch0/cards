1a32e9085900a0944af2b364e6b223324abbc2a0
desch0 <desch05@proton.me>
2023-09-11 00:52:38 +0300

    dev-0.1

--- app/src/main/java/cards/Main.java
+++ app/src/main/java/cards/Main.java
@@ -14,10 +14,37 @@ public class Main extends Template {
                 case "hand":
                     takeHand(Integer.parseInt(cmd[1]));
                     break;
+                case "take":
+                    takeHand(cmd[1], Integer.parseInt(cmd[2]));
+                    break;
                 case "exit": return;
             }
 
         }
+    }
+
+    public static void takeHand(String combination, int handLength) {
+        Hand hand = new Hand(handLength);
+      
+        int times;
+        switch(combination) {
+            case "set":     
+                while(true) {
+                    hand = new Hand(handLength);
+                    times++;
+                    if(hand.isSet()) break;
+                }
+                break;
+            case "quads":     
+                while(true) {
+                    hand = new Hand(handLength);
+                    times++;
+                    if(hand.isQuads()) break;
+                }
+                break;
+            
+        }
+        println(hand+" from the "+times+"th time.");
     }
 
     public static void takeHand(int handLength) {
